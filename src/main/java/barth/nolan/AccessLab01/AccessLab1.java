aaaddc31810c0200cc27d227314d122920da4e86
nolan <nolan.barth@gmail.com>
2016-01-25 11:01:52 -0500

    First commit

---
+++ src/main/java/barth/nolan/AccessLab01/AccessLab1.java
@@ -0,0 +1,9 @@
+package barth.nolan.AccessLab01;
+
+/**
+ * Created by Nolan Barth on 1/25/16.
+ */
+public class AccessLab1 {
+    
+
+}
---
+++ src/test/java/barth/nolan/AccessLab01/SpecAccessLab1.java
@@ -0,0 +1,6 @@
+package barth.nolan.AccessLab01;
+/**
+ * Created by Nolan Barth on 1/25/16.
+ */
+public class SpecAccessLab1 {
+}
