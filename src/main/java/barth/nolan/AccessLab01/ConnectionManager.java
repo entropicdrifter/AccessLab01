f8e628caa93b7d42ad0278be0ad26d622ef17d26
nolan <nolan.barth@gmail.com>
2016-01-25 11:35:16 -0500

    Added Java files

---
+++ src/main/java/barth/nolan/AccessLab01/ConnectionManager.java
@@ -0,0 +1,26 @@
+package barth.nolan.AccessLab01;
+
+/**
+ * Created by Nolan Barth on 1/25/16.
+ */
+public class ConnectionManager {
+    static Connection getConnection(String name){
+      return null;
+    }
+    static void newConnection(String name){
+
+    }
+    static void deleteConnection(String name){
+
+    }
+}
+class Connection{
+    private String portNum;
+    private String iPAddress;
+    private String getiPAddress(){
+        return null;
+    }
+    private String getPortNum(){
+        return null;
+    }
+}
